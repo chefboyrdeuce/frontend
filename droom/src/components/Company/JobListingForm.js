970839fe64d039c4d87f6b4577e5bba893cb4a25
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-13 21:08:34 -0500

    created components and folder to match the mock we are currently working on

---
+++ droom/src/components/Company/JobListingForm.js
@@ -0,0 +1,2 @@
+//this is a form where a company can add a new job listing with the 
+//require details from the end points
--- droom/src/index.js
+++ droom/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+// react router dom
+import { BrowserRouter as Router } from 'react-router-dom'
+//redux 
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+//redux store
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+, document.getElementById('root'));
 
