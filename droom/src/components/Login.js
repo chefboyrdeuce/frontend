02cbcf32b68347779d7ea11c1b5a175885b428ac
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-15 14:16:31 -0500

    added a blank api, added a value and OnChange to the form

--- droom/src/components/Login.js
+++ droom/src/components/Login.js
@@ -1,19 +1,56 @@
-import React from 'react';
-imp
+import React, { useState } from 'react';
+import api from '../utils/api';
+import { tsPropertySignature } from '@babel/types';
 
-function Signin() {
+function Signin(props) {
+    const [userCredentials, setUserCredential] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        setUserCredential({
+            ...userCredentials,
+            [e.target.name]: e.target.value,
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //created blank api call waiting for the backend to fill in
+        api()
+            .post("#", userCredentials)
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+                props.history.push('#')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return(
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Sign In Here</h1>
             <select>
                 <option value='employee'>Job-Seeker</option>
                 <option valer='empolyer'>Employer</option>
             </select>
             <br />
-            <input type='email' name='email' placeholder='Email' />
+            <input 
+                type='email' 
+                name='email' 
+                placeholder='Email' 
+                value={userCredentials.email} 
+                onChange={handleChange} 
+            />
             <br/>
-            <input type='password' name='password' placeholder='Password' />
+            <input 
+                type='password' 
+                name='password' 
+                placeholder='Password' 
+                value={userCredentials.email} 
+                onChange={handleChange} 
+            />
             <br/>
             <button type='submit'>Sign In</button> 
         </form>
