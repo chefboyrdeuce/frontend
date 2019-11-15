eafed709eee87ec7d89319b689339b1931b6f2a1
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-15 16:58:52 -0500

    setting up redux store

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { withRouter } from "react-router-dom"
 import './App.css';
+
 import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">      
-      <MainPage />      
+      <MainPage />
+      
     </div>
   );
 }
--- droom/src/components/MainPage.js
+++ droom/src/components/MainPage.js
@@ -3,9 +3,9 @@ import Nav from './Nav';
 
 function MainPage() {
     return (
-        <div>
-           
+        <div>           
             <Nav />
+            <h3>Welocome to the number one Job matcher on the web</h3>
         </div>
         
     )
